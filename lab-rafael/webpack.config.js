64c9aaf467500a9a9e302130567429191dcd9d65
Rafael Malave <rafomalave@gmail.com>
2018-04-21 11:11:11 -0700

    push bug for michael to look at

--- lab-rafael/webpack.config.js
+++ lab-rafael/webpack.config.js
@@ -1,12 +1,21 @@
+const HtmlPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
-  entry: './src/app.js',
+  devtool: 'source-map',
+  entry: __dirname + '/src/app.js',
+  output: {
+    path: __dirname + '/dist/',
+    filename: 'bundle-[hash].js'
+  },
+  plugins: [new HtmlPlugin({template: './index.html'})],
   module: {
     rules: [
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
       {
-        test: /\.js$/, loader: 'babel-loader'
+        test: /\.js$/, loader: 'babel-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.css$/, loader: ['style-loader', 'css-loader']
